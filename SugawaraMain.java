198e61f4b951bb326e1f127d5e98b56f7a4cc2eb
pommebarque-megumi <cache2gecko@gmail.com>
2022-04-01 09:47:58 +0900

    スコア比較

---
+++ SugawaraMain.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class SugawaraMain{
+	public static void main(String[] args){
+		//人数を聞いて、名前と点数を入れるための配列を作る
+		System.out.print("何人のスコアを比べますか？>>");
+		int n = new Scanner(System.in).nextInt();
+		int[] scores = new int[n];
+		String[] names = new String[n];
+		
+		//名前と点数を順番に入力してもらうfor
+		for(int i=0 ; i<n ; i++){
+			System.out.printf("%d人目の名前を入力>>",i+1);
+			String name = new Scanner(System.in).next();
+			names[i] = name;
+			System.out.printf("%d人目の点数を入力>>",i+1);
+			int score = new Scanner(System.in).nextInt();
+			scores[i] = score;
+		}
+		//名前と点数を同時に入れ替え
+		for(int i=0 ; i<scores.length-1 ; i++){
+			for(int j=i+1 ; j<scores.length ; j++){
+				if(scores[i]<scores[j]){
+					int temp = scores[i];
+					scores[i] = scores[j];
+					scores[j] = temp;
+					String keep = names[i];
+					names[i] = names[j];
+					names[j] = keep;
+				}
+			}
+		}
+		//結果表示names:(scores)
+		System.out.println("--　結果　--");
+		for(int i=0; i<n ;i++){
+			System.out.printf("(1):%s (%d)%n",names[i],scores[i]);
+		}
+	}
+}

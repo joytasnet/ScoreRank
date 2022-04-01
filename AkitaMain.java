cafa88022b7df7a13bb2bbff2a922e61c22e4568
fibuwimo <ray-rem-craim-requiem@ezweb.ne.jp>
2022-04-01 09:37:03 +0900

    ScoreRank修正

--- AkitaMain.java
+++ AkitaMain.java
@@ -11,6 +11,18 @@ public class AkitaMain{
 			System.out.printf("%d人目の点数>>",i+1);
 			scores[i] = new Scanner(System.in).nextInt();
 		}
+		for(int i=0 ;i<scores.length-1;i++){
+			for(int j=i+1;j<scores.length;j++){
+				if(scores[i]<scores[j]){
+					int tempS = scores[i];
+					scores[i] = scores[j];
+					scores[j] = tempS;
+					String tempN = names[i];
+					names[i] = names[j];
+					names[j] = tempN;
+				}
+			}
+		}
 		System.out.println("--結果--");
 		for(int i =0;i<n;i++){
 			System.out.printf("(%d)%s(%d)%n",i+1,names[i],scores[i]);
